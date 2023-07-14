023c92db26f380738eefba48f23d02f7c3d3ca52
imam-hossain <zunayedemon6t9@gmail.com>
2023-07-14 20:51:17 +0600

    setup env file

--- src/firebase/firebase.config.ts
+++ src/firebase/firebase.config.ts
@@ -2,12 +2,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBfZHP0KDAcTYdns3lsqQ11Y3UDy0wH3aw",
-  authDomain: "book-catalog-c9f30.firebaseapp.com",
-  projectId: "book-catalog-c9f30",
-  storageBucket: "book-catalog-c9f30.appspot.com",
-  messagingSenderId: "300828800405",
-  appId: "1:300828800405:web:af558bfe4325fdc312329c"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
 };
 
 const app = initializeApp(firebaseConfig);
